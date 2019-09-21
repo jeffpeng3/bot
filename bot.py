26259711de295820ff82483f3e7abd9ffbb9fc35
jeffpeng3 <54392299+jeffpeng3@users.noreply.github.com>
2019-09-21 11:03:00 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,14 +5,14 @@ import json
 with open('Setting.json','r',encoding='utf8') as jfile:
     jdata = json.load(jfile)
 
-bot = commands.Bot(command_prefix='!')
+bot = commands.Bot(command_prefix='*')
 
 @bot.event
 async def on_ready():
     print(">> bot is online <<")
 
 @bot.command()
-async def ping(ctx):
-    await ctx.send('pong')
+async def QQ(ctx):
+    await ctx.send('是在哭是吧')
                    
 bot.run(jdata['token'])

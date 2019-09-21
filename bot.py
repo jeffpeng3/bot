f417bca8f4af65085768b9f40a56e0dc5d3ad4a2
jeffpeng3 <54392299+jeffpeng3@users.noreply.github.com>
2019-09-21 10:31:15 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,11 @@
-print("hello")
+import discord
+from discord.ext import commands
+
+bot = commands.Bot(command_prefix='>')
+
+@bot.event
+async def on_ready():
+    print(">> bot is online <<")
+
+                   
+bot.run('NjI0NjMxMjc5NDEzMTAwNTc0.XYT6UA.eTPOGoRm2u0j7PgJJrClDgmT8LI')

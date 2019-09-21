790a981bb78f365afbdfe9f250d21a7f673ed025
jeffpeng3 <54392299+jeffpeng3@users.noreply.github.com>
2019-09-21 16:30:20 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,18 +1,36 @@
 import discord
 from discord.ext import commands
 import json
+import os
 
 with open('Setting.json','r',encoding='utf8') as jfile:
     jdata = json.load(jfile)
 
-bot = commands.Bot(command_prefix='*')
+bot = commands.Bot(command_prefix='!')
 
 @bot.event
 async def on_ready():
     print(">> bot is online <<")
 
 @bot.command()
-async def QQ(ctx):
-    await ctx.send('是在哭是吧')
-                   
-bot.run(jdata['token'])
+async def load(ctx, extension):
+    bot.load_extension(f'cmds.{extension}')
+    await ctx.send(f'load {extension} done.')
+
+@bot.command()
+async def unload(ctx, extension):
+    bot.unload_extension(f'cmds.{extension}')
+    await ctx.send(f'unload {extension} done.')
+
+@bot.command()
+async def reload(ctx, extension):
+    bot.reload_extension(f'cmds.{extension}')
+    await ctx.send(f'reload {extension} done.')
+
+
+for filename in os.listdir('./cmds'):
+    if filename.endswith('.py'):
+        bot.load_extension(f'cmds.{filename[:-3]}')
+
+if __name__ == "__main__":
+    bot.run(jdata['token'])
